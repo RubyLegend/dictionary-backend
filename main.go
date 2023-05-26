60051b3c0903284299fb4f6c3d4d8688686fe9dc
Serhii Hatsan <shatsanik01@office365.fiot.kpi.ua>
2023-05-26 22:17:11 +0300

    Integrated database connection into Users repo

--- main.go
+++ main.go
@@ -1,62 +1,66 @@
 package main
 
 import (
-  "log"
-  "net/http"
+	"log"
+	"net/http"
 
-  "github.com/julienschmidt/httprouter"
+	"github.com/julienschmidt/httprouter"
 
-  "github.com/RubyLegend/dictionary-backend/routes"
-  "github.com/RubyLegend/dictionary-backend/routes/user"
-  "github.com/RubyLegend/dictionary-backend/routes/word"
-  "github.com/RubyLegend/dictionary-backend/routes/translation"
-  "github.com/RubyLegend/dictionary-backend/routes/dictionary"
-  "github.com/RubyLegend/dictionary-backend/routes/history"
-  "github.com/RubyLegend/dictionary-backend/routes/quiz"
+	"github.com/RubyLegend/dictionary-backend/routes"
+	dictionaryRoutes "github.com/RubyLegend/dictionary-backend/routes/dictionary"
+	historyRoutes "github.com/RubyLegend/dictionary-backend/routes/history"
+	quizRoutes "github.com/RubyLegend/dictionary-backend/routes/quiz"
+	translationRoutes "github.com/RubyLegend/dictionary-backend/routes/translation"
+	userRoutes "github.com/RubyLegend/dictionary-backend/routes/user"
+	wordRoutes "github.com/RubyLegend/dictionary-backend/routes/word"
 
-  userHelper "github.com/RubyLegend/dictionary-backend/middleware/users"
+	db "github.com/RubyLegend/dictionary-backend/middleware/database"
+	userHelper "github.com/RubyLegend/dictionary-backend/middleware/users"
 )
 
 func main() {
-  // Creating http router
-  router := httprouter.New()
+	// Connecting to database
+	db.OpenConnection()
 
-  router.GET("/", routes.Index)
+	// Creating http router
+	router := httprouter.New()
 
-  router.POST("/api/v1/word", wordRoutes.WordPost)
-  router.DELETE("/api/v1/word/:id", wordRoutes.WordDelete)
-  router.GET("/api/v1/word", wordRoutes.WordGet)
-  router.PATCH("/api/v1/word/:id", wordRoutes.WordPatch)
+	router.GET("/", routes.Index)
 
-  router.POST("/api/v1/translation", translationRoutes.TranslationPost)
-  router.DELETE("/api/v1/translation/:id", translationRoutes.TranslationDelete)
-  router.GET("/api/v1/translation", translationRoutes.TranslationGet)
-  router.PATCH("/api/v1/translation/:id", translationRoutes.TranslationPatch)
+	router.POST("/api/v1/word", wordRoutes.WordPost)
+	router.DELETE("/api/v1/word/:id", wordRoutes.WordDelete)
+	router.GET("/api/v1/word", wordRoutes.WordGet)
+	router.PATCH("/api/v1/word/:id", wordRoutes.WordPatch)
 
-  router.POST("/api/v1/user/login", userRoutes.UserLogin)
-  router.POST("/api/v1/user/signup", userRoutes.UserSignup)
-  router.POST("/api/v1/user/logout", userRoutes.UserLogout)
-  router.GET("/api/v1/user/status", userRoutes.UserStatus)
-  router.POST("/api/v1/user/restore-username", userRoutes.UserRestoreUsername)
-  router.POST("/api/v1/user/restore-password", userRoutes.UserRestorePassword)
-  router.DELETE("/api/v1/user", userRoutes.UserDelete)
-  router.PATCH("/api/v1/user", userRoutes.UserPatch)
-  
-  router.GET("/api/v1/dictionary", dictionaryRoutes.DictionaryGet)
-  router.POST("/api/v1/dictionary", dictionaryRoutes.DictionaryPost)
-  router.PATCH("/api/v1/dictionary/:id", dictionaryRoutes.DictionaryPatch)
-  router.DELETE("/api/v1/dictionary/:id", dictionaryRoutes.DictionaryDelete)
+	router.POST("/api/v1/translation", translationRoutes.TranslationPost)
+	router.DELETE("/api/v1/translation/:id", translationRoutes.TranslationDelete)
+	router.GET("/api/v1/translation", translationRoutes.TranslationGet)
+	router.PATCH("/api/v1/translation/:id", translationRoutes.TranslationPatch)
 
-  router.GET("/api/v1/history", historyRoutes.HistoryGet)
-  router.DELETE("/api/v1/history", historyRoutes.HistoryDelete)
+	router.POST("/api/v1/user/login", userRoutes.UserLogin)   // done
+	router.POST("/api/v1/user/signup", userRoutes.UserSignup) // done
+	router.POST("/api/v1/user/logout", userRoutes.UserLogout) // done
+	router.GET("/api/v1/user/status", userRoutes.UserStatus)  // done
+	router.POST("/api/v1/user/restore-username", userRoutes.UserRestoreUsername)
+	router.POST("/api/v1/user/restore-password", userRoutes.UserRestorePassword)
+	router.DELETE("/api/v1/user", userRoutes.UserDelete) // done
+	router.PATCH("/api/v1/user", userRoutes.UserPatch)   // done
 
-  router.GET("/api/v1/quiz/new", quizRoutes.QuizGetNew)
-  router.POST("/api/v1/quiz/:quizId", quizRoutes.QuizPost)
-  router.GET("/api/v1/quiz/status", quizRoutes.QuizGetStatus)
+	router.GET("/api/v1/dictionary", dictionaryRoutes.DictionaryGet)
+	router.POST("/api/v1/dictionary", dictionaryRoutes.DictionaryPost)
+	router.PATCH("/api/v1/dictionary/:id", dictionaryRoutes.DictionaryPatch)
+	router.DELETE("/api/v1/dictionary/:id", dictionaryRoutes.DictionaryDelete)
 
-  // Just for now logout monitor will detach to it's own thread here
-  go userHelper.LogoutMonitor()
+	router.GET("/api/v1/history", historyRoutes.HistoryGet)
+	router.DELETE("/api/v1/history", historyRoutes.HistoryDelete)
 
-  log.Println("Server started at http://localhost:8080")
-  log.Fatal(http.ListenAndServe(":8080", router))
+	router.GET("/api/v1/quiz/new", quizRoutes.QuizGetNew)
+	router.POST("/api/v1/quiz/:quizId", quizRoutes.QuizPost)
+	router.GET("/api/v1/quiz/status", quizRoutes.QuizGetStatus)
+
+	// Just for now logout monitor will detach to it's own thread here
+	go userHelper.LogoutMonitor()
+
+	log.Println("Server started at http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
