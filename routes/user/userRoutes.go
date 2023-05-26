e75c41d4abcdd56322686cf9a4b23511102c0fce
Serhii Hatsan <shatsanik01@office365.fiot.kpi.ua>
2023-05-26 22:17:49 +0300

    Removed debug messages, fix to PATCH method
    Main fix is: logged out JWT token after editing user data.
    Also in previous commit were changed some logic:
    * To edit user info, password is required, and it must match current
      password. Because of that, password won't be changed so easily.
    
    For password modification there will be created separate endpoint, which
    will handle password change.

--- routes/user/userRoutes.go
+++ routes/user/userRoutes.go
@@ -1,179 +1,173 @@
 package userRoutes
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
-	"encoding/json"
-  "errors"
-  
+
 	userHelper "github.com/RubyLegend/dictionary-backend/middleware/users"
 	userRepo "github.com/RubyLegend/dictionary-backend/repository/users"
-  
+
 	"github.com/julienschmidt/httprouter"
 )
 
 func UserLogin(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-  w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 
-  var userData userRepo.User
-  _ = json.NewDecoder(r.Body).Decode(&userData)
-  resp := make(map[string]any)
+	var userData userRepo.User
+	_ = json.NewDecoder(r.Body).Decode(&userData)
+	resp := make(map[string]any)
 
-    
-  if userData.Username == "" {
-    resp["error"] = []string{errors.New("Username not provided. Cannot authorize.").Error()}
-    w.WriteHeader(http.StatusNotFound)
-  } else {
-    user, err := userHelper.VerifyCredentials(userData)
-    if err != nil {
-      var errors []string
-      for _, v := range err {
-        errors = append(errors, v.Error())
-      }
-      resp["error"] = errors
-      w.WriteHeader(http.StatusForbidden)
-    } else {
-      token, err := userHelper.GenerateJWT(userData.Username)
+	if userData.Username == "" {
+		resp["error"] = []string{errors.New("Username not provided. Cannot authorize.").Error()}
+		w.WriteHeader(http.StatusNotFound)
+	} else {
+		user, err := userHelper.VerifyCredentials(userData)
+		if err != nil {
+			var errors []string
+			for _, v := range err {
+				errors = append(errors, v.Error())
+			}
+			resp["error"] = errors
+			w.WriteHeader(http.StatusForbidden)
+		} else {
+			token, err := userHelper.GenerateJWT(userData.Username)
 
-      if err != nil {
-        resp["error"] = []string{err.Error()}
-        w.WriteHeader(http.StatusBadGateway)
-      } else {
-        resp["token"] = token
-        resp["userData"] = user
-        w.WriteHeader(http.StatusOK)
-      }
-    }
-  }
-  _ = json.NewEncoder(w).Encode(resp)
-  fmt.Fprintf(w, "Under Construction\n")
+			if err != nil {
+				resp["error"] = []string{err.Error()}
+				w.WriteHeader(http.StatusInternalServerError)
+			} else {
+				resp["token"] = token
+				resp["userData"] = user
+				w.WriteHeader(http.StatusOK)
+			}
+		}
+	}
+	_ = json.NewEncoder(w).Encode(resp)
 }
-  
+
 func UserSignup(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-  w.Header().Set("Content-Type", "application/json")
-  var userData userRepo.User
-  _ = json.NewDecoder(r.Body).Decode(&userData)
-  resp := make(map[string]any)
+	w.Header().Set("Content-Type", "application/json")
+	var userData userRepo.User
+	_ = json.NewDecoder(r.Body).Decode(&userData)
+	resp := make(map[string]any)
 
-  err := userRepo.AddUser(userData)
-  
-  if err != nil {
-    var errors []string
-    for _, v := range err {
-      errors = append(errors, v.Error())
-    }
+	err := userRepo.AddUser(userData)
 
-    resp["error"] = errors
-    w.WriteHeader(http.StatusNotAcceptable)
-  } else {
-    resp["status"] = "User added successfully"
-  }
-    
-  _ = json.NewEncoder(w).Encode(resp)
-  fmt.Fprintf(w, "Under Construction\n")
-}
-  
-func UserLogout(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-  w.Header().Set("Content-Type", "application/json")
-  resp := make(map[string]any)
+	if err != nil {
+		var errors []string
+		for _, v := range err {
+			errors = append(errors, v.Error())
+		}
 
-  userHelper.LogoutJWT(w,r,resp)
-
-  _ = json.NewEncoder(w).Encode(resp)
-  fmt.Fprintf(w, "Not Implemented\n")
-}
-  
-func UserStatus(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-  w.Header().Set("Content-Type", "application/json")
-  var userData userRepo.User
-  resp := make(map[string]any)
-
-  claims := userHelper.VerifyJWT(w, r, resp)
-  if resp["error"] == nil {
-        userData.Username = claims["username"].(string)
-        userData, err := userRepo.GetUser(userData)
-
-		    if err != nil {
-          resp["error"] = []string{err.Error()}
-          w.WriteHeader(http.StatusNotFound)
-        } else {
-          resp["claims"] = claims
-          userData.Password = ""
-          resp["userData"] = userData
-        }
+		resp["error"] = errors
+		w.WriteHeader(http.StatusNotAcceptable)
+	} else {
+		resp["status"] = "User added successfully"
 	}
 
-  _ = json.NewEncoder(w).Encode(resp)
-	fmt.Fprintf(w, "Under Construction\n")
+	_ = json.NewEncoder(w).Encode(resp)
 }
-  
+
+func UserLogout(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json")
+	resp := make(map[string]any)
+
+	userHelper.LogoutJWT(w, r, resp)
+
+	_ = json.NewEncoder(w).Encode(resp)
+}
+
+func UserStatus(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json")
+	var userData userRepo.User
+	resp := make(map[string]any)
+
+	claims := userHelper.VerifyJWT(w, r, resp)
+	if resp["error"] == nil {
+		userData.Username = claims["username"].(string)
+		userData, err := userRepo.GetUser(userData)
+
+		if err != nil {
+			resp["error"] = []string{err.Error()}
+			w.WriteHeader(http.StatusNotFound)
+		} else {
+			resp["claims"] = claims
+			userData.Password = ""
+			resp["userData"] = userData
+		}
+	}
+
+	_ = json.NewEncoder(w).Encode(resp)
+}
+
 func UserRestoreUsername(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintf(w, "Not Implemented\n")
 }
-  
+
 func UserRestorePassword(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintf(w, "Not Implemented\n")
 }
-  
+
 func UserDelete(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-  w.Header().Set("Content-Type", "application/json")
-  var userData userRepo.User
-  resp := make(map[string]any)
+	w.Header().Set("Content-Type", "application/json")
+	var userData userRepo.User
+	resp := make(map[string]any)
 
-  claims := userHelper.VerifyJWT(w,r,resp)
+	claims := userHelper.VerifyJWT(w, r, resp)
 
-  if resp["error"] == nil {
-    userData.Username = claims["username"].(string)
-    err := userRepo.DeleteUser(userData)
+	if resp["error"] == nil {
+		userData.Username = claims["username"].(string)
+		err := userRepo.DeleteUser(userData)
 
-    if err == nil {
-      resp["status"] = "Success"
-    } else {
-      resp["status"] = "Failed"
-      resp["error"] = []string{err.Error()}
-    }
-  }
+		if err == nil {
+			resp["status"] = "Success"
+		} else {
+			resp["status"] = "Failed"
+			resp["error"] = []string{err.Error()}
+		}
+	}
 
-  _ = json.NewEncoder(w).Encode(resp)
-	fmt.Fprintf(w, "Under Construction\n")
+	_ = json.NewEncoder(w).Encode(resp)
 }
-  
+
 func UserPatch(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-  w.Header().Set("Content-Type", "application/json")
-  var userData userRepo.User
-  _ = json.NewDecoder(r.Body).Decode(&userData)
-  resp := make(map[string]any)
+	w.Header().Set("Content-Type", "application/json")
+	var userData userRepo.User
+	_ = json.NewDecoder(r.Body).Decode(&userData)
+	resp := make(map[string]any)
 
-  claims := userHelper.VerifyJWT(w,r,resp)
-  
-  if resp["error"] == nil {
-    username := claims["username"].(string)
-    err := userRepo.EditUser(username, userData)
+	claims := userHelper.VerifyJWT(w, r, resp)
 
-    if err == nil {
-      resp["status"] = "Success"
-      token, err := userHelper.GenerateJWT(userData.Username)
+	if resp["error"] == nil {
+		username := claims["username"].(string)
+		err := userRepo.EditUser(username, userData)
 
-      if err != nil {
-        resp["error"] = []string{err.Error()}
-        w.WriteHeader(http.StatusBadGateway)
-      } else {
-        resp["token"] = token
-        w.WriteHeader(http.StatusOK)
-      }
+		if err == nil {
+			resp["status"] = "Success"
+			userHelper.LogoutJWT(w, r, resp)
+			token, err := userHelper.GenerateJWT(userData.Username)
 
-    } else {
-      resp["status"] = "Failed"
-      var errors []string
-      for _, v := range err {
-        errors = append(errors, v.Error())
-      }
-      resp["error"] = errors
-      w.WriteHeader(http.StatusNotAcceptable)
-    }
+			if err != nil {
+				resp["error"] = []string{err.Error()}
+				w.WriteHeader(http.StatusBadGateway)
+			} else {
+				resp["token"] = token
+				w.WriteHeader(http.StatusOK)
+			}
 
-  }
+		} else {
+			resp["status"] = "Failed"
+			var errors []string
+			for _, v := range err {
+				errors = append(errors, v.Error())
+			}
+			resp["error"] = errors
+			w.WriteHeader(http.StatusNotAcceptable)
+		}
 
-  _ = json.NewEncoder(w).Encode(resp)
-	fmt.Fprintf(w, "Under Construction\n")
+	}
+
+	_ = json.NewEncoder(w).Encode(resp)
 }
