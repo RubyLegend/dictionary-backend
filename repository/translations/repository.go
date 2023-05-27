7d4d36ba096a821a77ccbcb974d696f5f561583e
s4meb0dy <96389216+s4meb0dy@users.noreply.github.com>
2023-05-27 18:58:31 +0300

    add translation

---
+++ repository/translations/repository.go
@@ -0,0 +1,177 @@
+package translations
+
+import (
+	"errors"
+	"strconv"
+
+	wordsRepo "github.com/RubyLegend/dictionary-backend/repository/words"
+)
+
+type Translation struct {
+	TranslationId int    `json:"translaonId"`
+	WordId        int    `json:"wordId"`
+	Name          string `json:"name"`
+	Language      string `json:"language"`
+}
+
+var Translations []Translation
+
+func checkTranslationExistance(translationData Translation) []error {
+	var err []error
+
+	for _, v := range Translations {
+		if v.Name == translationData.Name && v.WordId == translationData.WordId {
+			err = append(err, errors.New("Translation "+translationData.Name+" already exists"))
+		}
+	}
+
+	return err
+}
+
+func validation(translationData Translation) []error {
+	var err []error
+
+	if len(translationData.Name) == 0 {
+		err = append(err, errors.New("Name is required field"))
+	}
+	found := false
+	for _, translation := range wordsRepo.Words {
+		if translation.WordId == translationData.WordId {
+			found = true
+			break
+		}
+	}
+	if !found {
+		err = append(err, errors.New("Occured some error"))
+	}
+	return err
+}
+func findTranslation(params ...interface{}) (int, error) {
+	translationData, ok := params[0].(Translation)
+	if ok {
+		for i, v := range Translations {
+			if v.Name == translationData.Name {
+				return i, nil
+			}
+		}
+	} else {
+		name, ok := params[0].(string)
+		if ok {
+			for i, v := range Translations {
+				if v.Name == name {
+					return i, nil
+				}
+			}
+		} else {
+			return -1, errors.New("Unknown parameter passed")
+		}
+	}
+
+	return -1, errors.New("Translation not found")
+}
+func GetTranslation(TranslationId int) ([]error, Translation) {
+	var err []error
+	Translations = append(Translations, Translation{TranslationId: 1, WordId: 1, Name: "ehgfwe", Language: "ehgfwe"})
+	var FinedTranslation Translation
+
+	for _, v := range Translations {
+		if v.TranslationId == TranslationId {
+			FinedTranslation = v
+		}
+	}
+
+	if (FinedTranslation == Translation{}) {
+		err = append(err, errors.New("Translation not found"))
+	}
+
+	return err, FinedTranslation
+}
+
+func AddTranslation(translationData Translation) []error {
+	var err []error
+
+	err = append(err, validation(translationData)...)
+	err = append(err, checkTranslationExistance(translationData)...)
+
+	if err == nil {
+		lastElementIndex := len(Translations) - 1
+		if lastElementIndex < 0 {
+			translationData.WordId = 0
+		} else {
+			translationData.WordId = Translations[lastElementIndex].WordId + 1
+		}
+
+		Translations = append(Translations, translationData)
+
+		return nil
+	} else {
+		return nil
+	}
+}
+
+func DeleteTranslation(WordId int, TranslationId string) []error {
+	var err []error
+	id, error := strconv.Atoi(TranslationId)
+
+	if error != nil {
+		err = append(err, errors.New("Invalid id params"))
+		return err
+	}
+
+	var isDeleted = false
+	for i, v := range Translations {
+		if v.WordId == WordId && v.TranslationId == id {
+			Translations = append(Translations[:i], Translations[i+1:]...)
+			isDeleted = true
+			break
+		}
+	}
+
+	if !isDeleted {
+		err = append(err, errors.New("Translation not found"))
+	}
+
+	return err
+}
+func updateValidation(translationData Translation) []error {
+	var err []error
+	var isFieldInRequest = false
+	if len(translationData.Name) != 0 {
+		isFieldInRequest = true
+	}
+	if len(translationData.Language) != 0 {
+		isFieldInRequest = true
+	}
+	if !isFieldInRequest {
+		err = append(err, errors.New("Incorrect body"))
+	}
+	return err
+}
+
+func UpdateTranslation(WordId int, TranslationId string, translationData Translation) ([]error, Translation) {
+	var err []error
+	id, error := strconv.Atoi(TranslationId)
+
+	if error != nil {
+		err = append(err, errors.New("Invalid id params"))
+	}
+
+	err = append(err, updateValidation(translationData)...)
+
+	var UpdatedTranslation Translation
+	if err == nil {
+		for i, v := range Translations {
+			if v.WordId == WordId && v.TranslationId == id {
+				Translations[i] = translationData
+				UpdatedTranslation = Translations[i]
+				break
+			}
+		}
+		if UpdatedTranslation == (Translation{}) {
+			err = append(err, errors.New("Translation not found"))
+		}
+	}
+
+	return err, UpdatedTranslation
+
+}
--- routes/translation/translationRoutes.go
+++ routes/translation/translationRoutes.go
@@ -1,26 +1,106 @@
 package translationRoutes
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
-  
-	"github.com/julienschmidt/httprouter"
-  )
 
-  
-	func TranslationGet(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-		fmt.Fprintf(w, "Not Implemented\n")
-  }
-  
-  func TranslationPost(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-		fmt.Fprintf(w, "Not Implemented\n")
-  }
-  
-  func TranslationDelete(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-		fmt.Fprintf(w, "Not Implemented\n")
-  }
-  
-  func TranslationPatch(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-		fmt.Fprintf(w, "Not Implemented\n")
-  }
-  
+	translationRepo "github.com/RubyLegend/dictionary-backend/repository/translations"
+	"github.com/julienschmidt/httprouter"
+)
+
+func TranslationGet(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+
+	w.Header().Set("Content-Type", "application/json")
+	var WordId int = 1
+
+	resp := make(map[string]any)
+
+	errors, translation := translationRepo.GetTranslation(WordId)
+
+	if len(errors) > 0 {
+		var errorMessages []string
+		for _, v := range errors {
+			errorMessages = append(errorMessages, v.Error())
+		}
+		resp["error"] = errorMessages
+		w.WriteHeader(http.StatusBadRequest)
+	} else {
+		resp["translation"] = translation
+		w.WriteHeader(http.StatusOK)
+	}
+	_ = json.NewEncoder(w).Encode(resp)
+
+}
+func TranslationPost(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var WordId = 1
+
+	var translationData translationRepo.Translation
+	_ = json.NewDecoder(r.Body).Decode(&translationData)
+
+	resp := make(map[string]any)
+	translationData.WordId = WordId
+
+	err := translationRepo.AddTranslation(translationData)
+	if err != nil {
+		var errors []string
+		for _, v := range err {
+			errors = append(errors, v.Error())
+		}
+		resp["error"] = errors
+		w.WriteHeader(http.StatusBadRequest)
+	} else {
+		w.WriteHeader(http.StatusOK)
+	}
+	_ = json.NewEncoder(w).Encode(resp)
+}
+
+func TranslationDelete(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json")
+	resp := make(map[string]any)
+
+	var WordId int = 2
+	TranslationId := p.ByName("id")
+
+	errors := translationRepo.DeleteTranslation(WordId, TranslationId)
+
+	if len(errors) > 0 {
+		var errorMessages []string
+		for _, v := range errors {
+			errorMessages = append(errorMessages, v.Error())
+		}
+		resp["error"] = errorMessages
+		w.WriteHeader(http.StatusBadRequest)
+	} else {
+		w.WriteHeader(http.StatusOK)
+	}
+	_ = json.NewEncoder(w).Encode(resp)
+}
+
+func TranslationPatch(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json")
+	resp := make(map[string]any)
+
+	var WordId int = 2
+	TranslationId := p.ByName("id")
+
+	var translationData translationRepo.Translation
+	_ = json.NewDecoder(r.Body).Decode(&translationData)
+
+	errors, UpdatedTranslation := translationRepo.UpdateTranslation(WordId, TranslationId, translationData)
+
+	if len(errors) > 0 {
+		var errorMessages []string
+		for _, v := range errors {
+			errorMessages = append(errorMessages, v.Error())
+		}
+		resp["error"] = errorMessages
+		w.WriteHeader(http.StatusBadRequest)
+	} else {
+		resp["translation"] = UpdatedTranslation
+		w.WriteHeader(http.StatusOK)
+	}
+	_ = json.NewEncoder(w).Encode(resp)
+
+}
